adf96954c2fcd205bd9764ec3d065676df17ef0f
Michelle Bence <16948138+michellecan@users.noreply.github.com>
2021-10-24 23:37:34 -0700

    created initial laravel posty project and addded tailwindcss

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,11 @@
+<!-- the @ symbol is blade syntax -->
+@extends('layouts.app')
+
+@section('content')
+   <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            Posts
+        </div>
+
+   </div>
+@endsection
